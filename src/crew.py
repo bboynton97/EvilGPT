725564380bf923616a54993f515c9d265dcf01d2
Braelyn Boynton <bboynton97@gmail.com>
2024-10-19 12:08:42 -0700

    setup crew

---
+++ src/crew.py
@@ -0,0 +1,22 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+import tools
+
+@CrewBase
+class EvilgptCrew():
+	"""evilgpt crew"""
+
+	# Agent definitions
+
+	# Task definitions
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Test crew"""
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.sequential,
+			verbose=True,
+			# process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+		)
