1298b1667847c2ffe25e16bcc031fbe04871c806
Braelyn Boynton <bboynton97@gmail.com>
2024-10-19 15:42:28 -0700

    add tools to agents

--- src/crew.py
+++ src/crew.py
@@ -7,30 +7,31 @@ import tools
 class EvilgptCrew():
     """evilgpt crew"""
 
-    # Agent definitions
-    @agent
-    def hacker(self) -> Agent:
-        return Agent(
-            config=self.agents_config['hacker'],
-            tools=[],  # add tools here or use `agentstack tools add <tool_name>
-            verbose=True
-        )
-
-    @agent
-    def software_engineer(self) -> Agent:
-        return Agent(
-            config=self.agents_config['software_engineer'],
-            tools=[],  # add tools here or use `agentstack tools add <tool_name>
-            verbose=True
-        )
-
-    @agent
-    def file_sorter(self) -> Agent:
-        return Agent(
-            config=self.agents_config['file_sorter'],
-            tools=[],  # add tools here or use `agentstack tools add <tool_name>
-            verbose=True
-        )
+	# Agent definitions
+	@agent
+	def hacker(self) -> Agent:
+	    return Agent(
+	        config=self.agents_config['hacker'],
+	        tools=[],  # add tools here or use `agentstack tools add <tool_name>
+	        verbose=True
+	    )
+	
+	@agent
+	def software_engineer(self) -> Agent:
+	    return Agent(
+	        config=self.agents_config['software_engineer'],
+	        tools=[],  # add tools here or use `agentstack tools add <tool_name>
+	        verbose=True
+	    )
+	
+	@agent
+	def file_sorter(self) -> Agent:
+	    return Agent(
+	        config=self.agents_config['file_sorter'],
+	        tools=[tools.FileReadTool(), tools.dir_search_tool],  # add tools here or use `agentstack tools add <tool_name>
+	        verbose=True
+	    )
+	
 
     # Task definitions
     @task
@@ -50,3 +51,14 @@ class EvilgptCrew():
         return Task(
             config=self.tasks_config['vuln_writer'],
         )
+
+	@crew
+	def crew(self) -> Crew:
+		"""Creates the Test crew"""
+		return Crew(
+			agents=self.agents, # Automatically created by the @agent decorator
+			tasks=self.tasks, # Automatically created by the @task decorator
+			process=Process.hierarchical,
+			verbose=True,
+			manager_llm=""
+		)
--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,2 +1,4 @@
 
-# tool import
+# tool imports
+from .file_read_tool import FileReadTool
+from .dir_search_tool import DirectorySearchTool
