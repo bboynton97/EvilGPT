be3ce2a14f019ac7d268c8e4a2d32fb0d4e3df0e
reibs <areibman@gmail.com>
2024-10-20 10:58:21 -0700

    1 task only

--- src/crew.py
+++ src/crew.py
@@ -52,23 +52,23 @@ class EvilgptCrew():
             config=self.tasks_config['file_sort'],
         )
 
-    @task
-    def machine_inspect(self) -> Task:
-        return Task(
-            config=self.tasks_config['machine_inspect'],
-        )
+    # @task
+    # def machine_inspect(self) -> Task:
+    #     return Task(
+    #         config=self.tasks_config['machine_inspect'],
+    #     )
 
-    @task
-    def vuln_ideator(self) -> Task:
-        return Task(
-            config=self.tasks_config['vuln_ideator'],
-        )
+    # @task
+    # def vuln_ideator(self) -> Task:
+    #     return Task(
+    #         config=self.tasks_config['vuln_ideator'],
+    #     )
 
-    @task
-    def vuln_writer(self) -> Task:
-        return Task(
-            config=self.tasks_config['vuln_writer'],
-        )
+    # @task
+    # def vuln_writer(self) -> Task:
+    #     return Task(
+    #         config=self.tasks_config['vuln_writer'],
+    #     )
 
     @crew
     def crew(self) -> Crew:
