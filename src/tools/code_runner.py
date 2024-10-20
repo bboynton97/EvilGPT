816d8d4ebb8bbdd3f3fb8a76df8ed90be28fd7c1
reibs <areibman@gmail.com>
2024-10-20 11:07:29 -0700

    remove decorator

--- src/tools/code_runner.py
+++ src/tools/code_runner.py
@@ -11,7 +11,7 @@ interpreter.auto_run = True
 interpreter.llm.model = "gpt-4o"
 
 
-@tool("Executor")
+@tool
 def execute_code(code: str):
     """A tool to execute code using Open Interpreter. Returns the output of the code."""
     result = interpreter.chat(f"execute this code with no changes: {code}")
